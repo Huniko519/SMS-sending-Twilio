390aa1b0a4db178e66e205d28555038f6c50c74d
Top_Web_Master <Astro_Ninja@eclipso.eu>
2020-11-27 23:09:56 -0800

    commit

---
+++ Facade/Twilio.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Lakshmaji\Twilio\Facade;
+ 
+use Illuminate\Support\Facades\Facade;
+ 
+/**
+ * Twilio - Facade to support integration with Laravel framework 
+ *
+ * @package  Twilio
+ * @version  1.2.2
+ * @author   lakshmaji <lakshmajee88@gmail.com>
+ */ 
+class Twilio extends Facade {
+ 
+    protected static function getFacadeAccessor() { return 'twilio'; }
+ 
+}
+// end of class Twilio
+// end of file Twilio.php
---
+++ TwilioServiceProvider.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace Lakshmaji\Twilio;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Twilio - ServicePrivider to support integration with Laravel framework 
+ *
+ * @package  Twilio
+ * @version  1.2.2
+ * @author   lakshmaji <lakshmajee88@gmail.com>
+ */ 
+
+class TwilioServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return   Twilio
+     * @version  1.2.2
+     * @author   lakshmaji <lakshmajee88@gmail.com>
+     */
+    public function register()
+    {
+        if (method_exists(\Illuminate\Foundation\Application::class, 'singleton')) {
+            $this->app->singleton('twilio', function($app) {
+                return new Twilio;
+            });
+        } else {
+            $this->app['twilio'] = $this->app->share(function($app) {
+                return new Twilio;
+            });
+        }
+        // $this->app['twilio'] = $this->app->share(function($app) {
+        //     return new Twilio;
+        // });
+    }
+}
+// end of TwilioServiceProvider class
+// end of file TwilioServiceProvider 
+
